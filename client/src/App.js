42c8e2b49f36f5013602a4215ff10b929354872f
Romenick Tester <romenick.tester@gmail.com>
2021-11-28 22:32:40 +0000

    client: create home page + display products UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
     return (
@@ -9,7 +10,7 @@ const App = () => {
             <Header />
             <main className="main py-3">
                 <Container>
-                    <h1>Welcome to Proshop</h1>
+                    <HomePage />
                 </Container>
             </main>
             <Footer />
---
+++ client/src/pages/HomePage/index.js
@@ -0,0 +1,23 @@
+import { Row, Col } from "react-bootstrap";
+
+import products from "../../assets/products";
+import Product from "./Product";
+
+const HomePageMain = () => {
+    return (
+        <>
+            <h1>Late Products</h1>
+            <Row>
+                {products.map(product => {
+                    return (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    )
+                })}
+            </Row>
+        </>
+    )
+}
+
+export default HomePageMain;
