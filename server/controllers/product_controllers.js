bcff4b4ee2b9049a441160f2872bb29c1250eab1
Romenick Tester <romenick.tester@gmail.com>
2021-12-13 20:38:34 +0000

    server: product routes refactor - fetch from mongodb atlas

--- server/controllers/product_controllers.js
+++ server/controllers/product_controllers.js
@@ -1,11 +1,38 @@
-import products from "../assets/data/products.js";
+import asyncHandler from "express-async-handler";
 
-const getProductsCtrl = (req, res) => {
-    const tempList = [...products];
+import Product from "../assets/models/Product.js";
 
-    res.status(200).json(tempList);
-}
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  public
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+});
+
+// @desc    Fetch product by ID
+// @route   GET /api/products/:product_id
+// @access  public
+const getProductByID = asyncHandler(async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.product_id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json(product);
+    } catch (err) {
+        if (err.kind === "ObjectId") {
+            return res.status(404).json({ message: "Product not found" })
+        }
+
+        console.error(err.message);
+    }
+});
 
 export {
-    getProductsCtrl
+    getProducts,
+    getProductByID
 };
--- server/routes/product_routes.js
+++ server/routes/product_routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 
-import { getProductsCtrl } from "../controllers/product_controllers.js";
+import { getProducts, getProductByID } from "../controllers/product_controllers.js";
 
 const Router = express.Router();
 
-Router.get("/", getProductsCtrl);
+Router.get("/", getProducts);
+
+Router.get("/:product_id", getProductByID);
 
 export default Router;
