5e4a7f26318d247ead065db687097a6ae17d34ec
Romenick Tester <romenick.tester@gmail.com>
2022-03-24 11:39:54 +0000

    server: user auth login route endpoint

--- server/controllers/auth_controllers.js
+++ server/controllers/auth_controllers.js
@@ -1,9 +1,27 @@
+import User from "../assets/models/User.js";
+import asyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
 
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
-const login = (req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body);
-};
+    const user = await User.findOne({ email });
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if (user && matchPassword) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null
+        });
+    } else {
+        res.status(401)
+        throw new Error("Invalid credentials!")
+    }
+});
 
 const register = (req, res) => {
     console.log(req.body);
