c324c6b64a7f3b23000e99e2b15b2b96d8f0091b
Romenick Tester <romenick.tester@gmail.com>
2022-04-05 00:57:29 +0100

    server: update user profile endpoint

--- server/controllers/user_controllers.js
+++ server/controllers/user_controllers.js
@@ -1,5 +1,7 @@
+import bcrypt from "bcryptjs";
 import asyncHandler from "express-async-handler";
 import User from "../assets/models/User.js";
+import generateToken from "../assets/utils/tokenGenerator.js";
 
 // @desc    Get user profile
 // @route   GET /api/user/profile
@@ -10,4 +12,33 @@ const userProfile = asyncHandler(async (req, res) => {
     res.status(200).json(user);
 });
 
-export { userProfile };
+// @desc    Update user profile
+// @route   PUT /api/user/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user.id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            const hashedPassword = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10));
+            user.password = hashedPassword || user.password;
+        };
+
+        const updatedUserProfile = await user.save();
+
+        res.status(200).json({
+            name: updatedUserProfile.name,
+            email: updatedUserProfile.email,
+            isAdmin: updatedUserProfile.isAdmin,
+            token: generateToken(updatedUserProfile._id)
+        });
+    } else {
+        res.status(404)
+        throw new Error("User not found!")
+    }
+});
+
+export { userProfile, updateUserProfile };
--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { userProfile } from "../controllers/user_controllers.js";
+import { userProfile, updateUserProfile } from "../controllers/user_controllers.js";
 import protect from "../assets/middlewares/protect.js";
 
 const router = express.Router();
 
-router.get("/profile", protect, userProfile);
+router.route("/profile")
+    .get(protect, userProfile)
+    .put(protect, updateUserProfile);
 
 export default router;
